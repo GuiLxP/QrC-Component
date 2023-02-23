09b2bdb673aa93642d1c528f721a0463eddba7c3
GuiLxP <guilhermematheus166@gmail.com>
2023-02-23 16:53:19 -0300

    Quase terminado.

--- src/components/Qrcode.js
+++ src/components/Qrcode.js
@@ -1,52 +1,54 @@
-import React from "react";
-import { useQRCode } from "next-qrcode";
-import { Box, ThemeProvider } from "@material-ui/core";
+import React, { useState, useRef } from "react";
+import QRCode from "react-qr-code";
 
 export default function Qrcode() {
-  const { SVG } = useQRCode();
+  const [valorqrCode, setValorqrCode] = useState("http://github.com/guilxp");
+  const canvasRef = useRef(null);
+
+  function downloadQRCode() {
+    const canvas = canvasRef.current;
+    const pngUrl = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = "qrcode.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
+
   return (
-    <ThemeProvider
-      theme={{
-        palette: {
-          primary: {
-            main: "#FFFFFF",
-            dark: "#ABCDEF",
-          },
-        },
-      }}
-    >
-      <div>
-        <Box
-          className="box shadow-2xl justify-center items-center flex"
-          sx={{
-            width: 492,
-            height: 920,
-            borderRadius: 60,
-            bgcolor: "primary.main",
-            "&:hover": {
-              opacity: [0.9, 0.8, 0.7],
-              backgroundColor: "primary.dark",
-            },
-          }}
-        >
-          <div className="rounded-full">
-            <SVG
-              text={"https://google.com/"}
-              options={{
-                level: "high",
-                margin: 10,
-                scale: 4,
-                width: 460,
-                color: {
-                  dark: "#FFFFFF",
-                  light: "#4583e7",
-                },
-              }}
-            />
-          </div>
-          <h1 className=""></h1>
-        </Box>
+    <>
+      <div className="bg-white rounded-lg shadow-lg p-4">
+        <div className="flex flex-col items-center">
+          {valorqrCode != "" && (
+            <canvas
+              id="qrcode"
+              ref={canvasRef}
+              defaultValue={valorqrCode}
+              className="bg-slate-200 rounded-lg shadow-sm py-10 m-2"
+            >
+              <QRCode value={valorqrCode} />
+            </canvas>
+          )}
+          <input
+            type="text"
+            onChange={(e) => {
+              setValorqrCode(e.target.value);
+            }}
+            className="input border-slate-600 border rounded-lg m-2 p-2"
+          />
+          <button
+            onClick={() => {
+              downloadQRCode();
+            }}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Download
+          </button>
+        </div>
       </div>
-    </ThemeProvider>
+    </>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className="bg-slate-200 h-screen w-screen justify-center flex text-center">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
